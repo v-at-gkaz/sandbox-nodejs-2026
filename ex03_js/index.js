8fff3108bfc6085a2e85d86dcff03265a710b6fa
Vladimir <v@gkaz.ru>
2026-02-28 12:39:08 +0300

    Add example dotenv

--- ex03_js/index.js
+++ ex03_js/index.js
@@ -1,3 +1,8 @@
+import { config } from "dotenv";
+config();
+
+import { env } from "node:process";
+
 import { Square, size1, size2, print } from "./lib.js";
 // const {Square, size1, size2, print } = require("./lib.js");
 
@@ -6,3 +11,5 @@ print('The area of mySquare is', mySquare.area());
 
 const mySquare2 = new Square(size2);
 print('The area of mySquare2 is', mySquare2.area());
+
+console.log("ENV TEST:", env.VAR_TEST01);
